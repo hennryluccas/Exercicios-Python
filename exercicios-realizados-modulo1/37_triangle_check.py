3b8b0259fed9260d3731146bd18c5214b0bff779
Hennry Luccas Duarte Ponce Pereira <hennrybusiness@gmail.com>
2026-07-25 21:12:12 -0300

    Update 37_triangle_check.py

--- exercicios-realizados-modulo1/37_triangle_check.py
+++ exercicios-realizados-modulo1/37_triangle_check.py
@@ -20,3 +20,16 @@ if soma_dois_menores > maior:
     print('PODE formar um triângulo')
 else:
     print('NÃO PODE formar um triângulo')
+
+'''
+O professor fez de uma forma muito mais simples:
+
+l1 = int(input('Digite o primeiro lado: '))
+l2 = int(input('Digite o segundo lado: '))
+l3 = int(input('Digite o terceiro lado: '))
+
+if l1 < l2 + l3 and l2 < l1 + l3 and l3 < l1 + l2:
+    print('PODE formar um triângulo')
+else:
+    print('NÃO PODE formar um triângulo')
+'''
