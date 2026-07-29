d67aa7c2d6a63d0d1fa78c96dfbd4d76c0b17ba8
Hennry Luccas Duarte Ponce Pereira <hennrybusiness@gmail.com>
2026-07-29 17:40:01 -0300

    Create 08_triangle_type.py

---
+++ exercicios-realizados-modulo2/08_triangle_type.py
@@ -0,0 +1,23 @@
+'''
+Refaça o desafio 037 dos triângulos, acrescentando
+o recurso de mostrar que tipo de triângulo será formado:
+
+- Equilátero: todos os lados iguais
+- Isósceles: dois lados iguais
+- Escaleno: todos os lados diferentes
+'''
+
+lado1 = int(input('Digite o primeiro lado: '))
+lado2 = int(input('Digite o segundo lado: '))
+lado3 = int(input('Digite o terceiro lado: '))
+
+if lado1 < lado2 + lado3 and lado2 < lado1 + lado3 and lado3 < lado1 + lado2:
+    print('Pode formar um triângulo')
+    if lado1 == lado2 == lado3:
+        print('Esse é um triângulo EQUILÁTERO (todos os lados iguais)')
+    elif lado1 != lado2 and lado2 != lado3 and lado3 != lado1:
+        print('Esse é um triângulo ESCALENO (todos os lados diferentes)')
+    else:
+        print('Esse é um triângulo ISÓSCELES (dois lados iguais)')
+else:
+    print('Não é possível formar um triângulo')
