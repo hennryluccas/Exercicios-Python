9277320f0e5fe90a29ac25f9fc01d447af53cd34
Hennry Luccas Duarte Ponce Pereira <hennrybusiness@gmail.com>
2026-07-28 21:46:51 -0300

    Create 05_military_enlistment.py

---
+++ exercicios-realizados-modulo2/05_military_enlistment.py
@@ -0,0 +1,25 @@
+'''
+Faça um programa que leia o ano de nascimento
+de um jovem e informe, de acordo com sua idade:
+
+- Se ele ainda vai se alistar ao serviço militar
+- Se é a hora de se alistar
+- Se já passou do tempo do alistamento
+
+Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
+'''
+
+from datetime import date
+ano_nascimento = int(input('Escreva o ano de nascimento: '))
+idade_servico_militar = 18
+idade_usuario = date.today().year - ano_nascimento
+falta_completar = idade_servico_militar - idade_usuario
+ultrapassou_idade = idade_usuario - idade_servico_militar
+
+
+if idade_usuario < idade_servico_militar:
+    print('Você só vai se alistar quando completar 18 anos, isso será daqui {} ano(s). FIQUE ATENTO!'.format(falta_completar))
+elif idade_usuario == idade_servico_militar:
+    print('Está na hora de se alistar! Vá pessoalmente a uma Junta de Serviço Militar mais próxima.')
+else:
+    print('Já passou da hora de você se alistar! Já se passaram {} ano(s)!'.format(ultrapassou_idade))
