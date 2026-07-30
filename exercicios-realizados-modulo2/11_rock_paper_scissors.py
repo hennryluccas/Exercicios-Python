1350e21a47992b0954ef36c81ae20d59df06c277
Hennry Luccas Duarte Ponce Pereira <hennrybusiness@gmail.com>
2026-07-30 18:57:15 -0300

    Update 11_rock_paper_scissors.py

--- exercicios-realizados-modulo2/11_rock_paper_scissors.py
+++ exercicios-realizados-modulo2/11_rock_paper_scissors.py
@@ -1,24 +1,32 @@
 '''
 Crie um programa que faça o computador jogar Jokenpô com você:
 '''
-
+from time import sleep
 from random import randint
-usuario = int(input('Escolha uma opção: \n1 - Pedra \n2 - Papel \n3 - Tesoura \nDigite aqui: '))
-lista = ['Pedra', 'Papel', 'Tesoura']
+usuario = int(input('Escolha uma opção: \n[ 0 ] Pedra \n[ 1 ] Papel \n[ 2 ] Tesoura \nDigite aqui: '))
+lista = ['PEDRA', 'PAPEL', 'TESOURA']
 computador = randint(0,2)
-print('Eu escolho {}'.format(lista[computador]))
-
-if usuario == 1 and computador == 1:
+print('JO')
+sleep(0.5)
+print('KEN')
+sleep(0.5)
+print('PÔ')
+sleep(0.5)
+print('-=' * 20)
+print('Computador jogou {}'.format(lista[computador]))
+print('Jogador jogou {}'.format(lista[usuario]))
+if usuario == 0 and computador == 1:
     print('Você PERDEU!')
+elif usuario == 0 and computador == 2:
+    print('Você GANHOU!')
+elif usuario == 1 and computador == 0:
+    print('Você GANHOU!')
 elif usuario == 1 and computador == 2:
-    print('Você GANHOU!')
+    print('Você PERDEU!')
 elif usuario == 2 and computador == 0:
-    print('Você GANHOU!')
-elif usuario == 2 and computador == 2:
     print('Você PERDEU!')
-elif usuario == 3 and computador == 0:
-    print('Você PERDEU!')
-elif usuario == 3 and computador == 1:
+elif usuario == 2 and computador == 1:
     print('Você GANHOU!')
 else:
     print('Empatou!')
+print('-=' * 20)
